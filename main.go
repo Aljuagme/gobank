b40e0ed6a62c4f06aa0931c3041fd3632c69a27c
Alvaro Juan Gomez <aljuagme@gmail.com>
2024-02-28 20:04:34 +0100

    comminting all

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func seedAccount(store Storage, fname, lname, pw string) *Account {
+	acc, err := NewAccount(fname, lname, pw)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := store.CreateAccount(acc); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("new account => ", acc.Number)
+
+	return acc
+}
+
+func seedAccounts(s Storage) {
+	seedAccount(s, "Alvaro", "Juan", "hunter")
+}
+
+func main() {
+	seed := flag.Bool("seed", false, "seed the db")
+	flag.Parse()
+
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	if *seed {
+		fmt.Println("seeding the database")
+		// seed stuff
+		seedAccounts(store)
+	}
+
+	server := NewAPIServer(":3000", store)
+	server.Run()
+}
